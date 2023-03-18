d8e24c0e8e132506488a851c4df5fdfc92cf744a
Sebastian Thunberg <sebthu@kth.se>
2023-03-18 17:57:37 +0100

    added missing comments

--- server/api/ApplicationApi.js
+++ server/api/ApplicationApi.js
@@ -12,6 +12,18 @@ class ApplicationApi extends RequestHandler {
 	}
 	async registerHandler() {
 		try {
+			/**
+         * create a new application for the logged in user. 
+         * @param personId : this parsmeter is the logged in user id for identifing the user.
+         * @param availability : this parsmeter displays the periods the applicat can work.
+         * @param competence : this parsmeter displays what competence the user have and how many years of experience they have.  
+         * 
+         * sends: 200 when the application is successfully created.
+         *        400 when any error occurs when creating the application or entering unvalidated data by the user.  
+         * returns: the created application. 
+         * 
+         * throws error if any error occurs when creating the application in the database. 
+         */
 			await this.retrieveController();
 			this.router.post('/create', async (req, res, next) => {
 				try {
@@ -33,6 +45,13 @@ class ApplicationApi extends RequestHandler {
 					next(err);
 				}
 			});
+				/**
+             * list all applications. 
+             * sends : 200 when having applications in database to send. 
+             *         400 when there are no applications in the database. 
+             * 
+             * returns a list of all applications. 
+             */
 			this.router.get('/list', async (req, res, next) => {
 				try {
 					const list = await this.contr.findAllApplications();
